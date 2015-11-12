f7b27364ce9b64d4d94264c920adbfe203d94c56
Sia <siakaramalegos@gmail.com>
2015-11-12 09:56:02 -0600

    Changed images to center and square crop and added file size validation

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,8 +4,14 @@ class Dog < ActiveRecord::Base
 
   validates :name, :breed_id, :owner_id, :dob, presence: true
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: {
+    medium: "300x300#",
+    thumb: "100x100#"
+    }, default_url: ":style_missing.jpg"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
+  validates_attachment_size :avatar, :less_than => 3.megabytes,
+    :unless => Proc.new {|m| m[:avatar_file_name].blank?}
 end
 
 # == Schema Information
