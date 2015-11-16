3e9d565ac22bd630f55b3c9505eb89e2b8b64109
Sia <siakaramalegos@gmail.com>
2015-11-16 10:40:26 -0600

    Added check in/out buttons to dog index

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -55,7 +55,7 @@ class DogsController < ApplicationController
   def update
     respond_to do |format|
       if @dog.update(dog_params)
-        format.html { redirect_to @dog, notice: 'Dog was successfully updated.' }
+        format.html { redirect_to dogs_path, notice: 'Dog was successfully updated.' }
         format.json { render :show, status: :ok, location: @dog }
       else
         format.html { render :edit }
