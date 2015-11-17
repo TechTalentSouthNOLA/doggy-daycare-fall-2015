043ea9ec89c9bdba8d41800af0686b84e59d0ff4
Sia <siakaramalegos@gmail.com>
2015-11-17 08:49:17 -0600

    Added user model and required authentication for dog and owner views

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,5 +1,6 @@
 class OwnersController < ApplicationController
   before_action :set_owner, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /owners
   # GET /owners.json
